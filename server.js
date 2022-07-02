7f922105a1ed3b0fefb04ec605a90c1867608757
Lindsey Sherrill <lindsey350@aol.com>
2022-07-02 11:46:57 -0400

    fix routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require('./note-taker/routes/htmlRoutes')
-const apiRoutes = require('./note-taker/routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
